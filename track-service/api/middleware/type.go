991d6c1abaa078eeb087231bd7cd3e91ed448802
strelcock <xxxvanesxxx673@gmail.com>
2025-11-06 19:32:19 +0300

    try parallel build and logs into middleware

--- track-service/api/middleware/type.go
+++ track-service/api/middleware/type.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"io"
+	"log"
 	"net/http"
 )
 
@@ -18,17 +19,17 @@ func WebhookEvent(next http.Handler) http.Handler {
 		rawData, err := io.ReadAll(r.Body)
 		if err != nil {
 			w.WriteHeader(500)
-			w.Write([]byte("read body" + err.Error()))
+			w.Write([]byte("read body: " + err.Error()))
 			return
 		}
-		//log.Println(rawData)
+		log.Println(rawData)
 
 		rawMap := make(map[string]map[string]any)
 		err = json.Unmarshal(rawData, &rawMap)
 		//log.Println(rawMap)
 		if err != nil {
 			w.WriteHeader(500)
-			w.Write([]byte("unmarshal into map" + err.Error()))
+			w.Write([]byte("unmarshal into map: " + err.Error()))
 			return
 		}
 		for k := range rawMap {
