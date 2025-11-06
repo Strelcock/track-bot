3f59502faa97b887133d63720098a1781f97695c
strelcock <xxxvanesxxx673@gmail.com>
2025-11-06 19:16:22 +0300

    add kafka conn retries

--- track-service/internal/infrastructure/kafka/kafka.go
+++ track-service/internal/infrastructure/kafka/kafka.go
@@ -3,6 +3,9 @@ package kafka
 import (
 	"context"
 	"fmt"
+	"log"
+	"math"
+	"time"
 
 	"github.com/segmentio/kafka-go"
 )
@@ -12,6 +15,18 @@ type Producer struct {
 }
 
 func New(brokers []string, topic string) *Producer {
+	for i := range 5 {
+		time.Sleep(time.Duration(math.Pow(2, float64(i))) * time.Second)
+		conn, err := kafka.DialLeader(context.Background(), "tcp", brokers[0], topic, 0)
+		if err == nil {
+			controller, _ := conn.Controller()
+			log.Print(controller.Host, controller.Port)
+			conn.Close()
+			break
+		} else if i == 4 {
+			log.Fatal(err)
+		}
+	}
 	return &Producer{
 		kafka.NewWriter(kafka.WriterConfig{
 			Brokers: brokers,
